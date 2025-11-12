ce2e820f71bc940391a7f456ad83ed3228d05953
farahalterman <108204840+farahalterman@users.noreply.github.com>
2025-11-11 21:29:59 -0800

    Add optional fields to profile

--- src/profile.js
+++ src/profile.js
@@ -15,11 +15,16 @@ function populateUserInfo() {
         if (userSnap.exists()) {
           const userData = userSnap.data();
 
-          const { displayName = "", uid = "", city = "" } = userData;
+          const { displayName = "", uid = "", pronouns = "" ,city = "" } = userData;
           console.log(userData)
-
+          //Populate Profile Page
           document.getElementById("name-goes-here").textContent = displayName;
           document.getElementById("userID-goes-here").textContent = uid;
+
+          //Populate Edit Page
+          document.getElementById("nameInput").value = displayName;
+          if (pronouns) document.getElementById("pronounInput").value = pronouns;
+          if (city) document.getElementById("cityInput").value = city;
           
         } else {
           console.log("No such document!");
@@ -37,12 +42,49 @@ function populateUserInfo() {
 populateUserInfo();
 
 //-------------------------------------------------------------
+// Toggle between views
+//-------------------------------------------------------------
+function setVisible(el, visible) {
+        el.classList.toggle('d-none', !visible);
+    }
+
+editBtn?.addEventListener("click", () => {
+  setVisible(profileView, false);
+  setVisible(editProfileView, true);
+} )
+
+discardEditBtn?.addEventListener("click", () => {
+  setVisible(editProfileView, false);
+  setVisible(profileView, true);
+});
+
+
+//-------------------------------------------------------------
 // Function to enable editing of user info form fields
 //------------------------------------------------------------- 
 document.querySelector('#editBtn').addEventListener('click', editUserInfo);
 function editUserInfo() {
     //Enable the form fields
     document.getElementById('personalInfoFields').disabled = false;
+}
+
+//-------------------------------------------------------------
+// Function to save updated user info from the profile form
+//-------------------------------------------------------------
+document.querySelector('#saveButton').addEventListener('click', saveUserInfo);   //Add event listener for save button
+async function saveUserInfo() {
+		  const user = auth.currentUser;   // ✅ get the currently logged-in user
+	    if (!user) {
+		    alert("No user is signed in. Please log in first.");
+		    return;
+		  }
+     //enter code here
+
+     //a) get user entered values
+
+     //b) update user's document in Firestore
+
+     //c) disable edit 
 }
 
 
